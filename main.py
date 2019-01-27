2cef14e293f517b5a103aa9c3232a673ee22347c
Mick <michalk2447@gmail.com>
2019-01-27 21:34:26 +0100

    merged conflicts resolved

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin//python3
 
 def main(msg):
-        // Adding a diffrent com
+        // Adding the second commen to this file.
+	// Print a message
         print (msg)
 
 main("Hello people")
